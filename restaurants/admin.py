4a857ae4f55c462a9fc49a76be5d64cbf9d9923d
Arsen <suttibaevrinat@gmail.com>
2026-08-20 12:06:16 +0500

    Configure Django admin

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Restaurant, Category, MenuItem
 
-# Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "city")
+    search_fields = ("name", "city")
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "restaurant")
+    search_fields = ("name", "restaurant__name")
+    list_filter = ("restaurant",)
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "category")
+    search_fields = ("name", "category__name")
+    list_filter = ("category",)
